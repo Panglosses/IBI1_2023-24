5d20ce6cd90afcdbe7e61cea05f1c7d68982abb3
lhx <1144788101@qq.com>
2024-05-02 23:33:35 +0800

    initialise

--- Practical6/citysizes.py
+++ Practical6/citysizes.py
@@ -3,6 +3,7 @@ chn_cities=[0.58,8.4,29.9,22.2]
 uk_name=["Edinburgh","Glasgow","Stirling","Longdon"]
 chn_name=["Haining","Hangzhou","Shanghai","Beijing"]
 import matplotlib.pyplot as plt
+
 plt.figure()
 plt.bar(chn_name,chn_cities,width=0.8)
 plt.ylabel("Population")
@@ -12,7 +13,7 @@ plt.show()
 
 plt.clf()
 
-plt.figure()
+
 plt.bar(uk_name,uk_cities,width=0.8)
 plt.ylabel("Population")
 plt.xlabel("Cities")
@@ -20,3 +21,9 @@ plt.title("cities'population of UK")
 plt.show()
 
 plt.clf()
+
+uk_cities_sorted = sorted((uk_cities), reverse=True)
+print("Sorted UK cities by population:", uk_cities_sorted)
+
+chn_cities_sorted = sorted((chn_cities), reverse=True)
+print("Sorted China cities by population:", chn_cities_sorted)
